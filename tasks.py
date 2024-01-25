705ccbdbcc58973032a211a878149a9067d6ad4b
Omar Shemy <OmarShemy@Omar-Shemys-MacBook-Pro.local>
2024-01-25 12:59:37 -0500

    tasks error wrap

--- tasks.py
+++ tasks.py
@@ -3,6 +3,7 @@ import smtplib
 from datetime import datetime
 from celery import Celery
 from main import app, db, Birthdays
+from twilio.rest import Client
 import os
 celery = Celery('tasks', broker=os.environ.get('R_KEY'))
 
@@ -27,17 +28,30 @@ def send_email_task():
                 message = messages[random.randint(0, 2)]
                 email = os.environ.get('E_ID')
                 password = os.environ.get('P_KEY')
-                with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
-                    connection.starttls()
-                    connection.login(user=email, password=password)
-                    connection.sendmail(from_addr=email,
-                                        to_addrs=recipient_email,
-                                        msg=f"Subject: Happy Birthday!\n\nHey {recipient_name},\n{message}{wisher_name}."
-                                        )
-                    connection.sendmail(from_addr=email,
-                                        to_addrs=wisher_email,
-                                        msg=f"Subject: Birthday Wish Sent.\n\nHey {wisher_name},\nA birthday wish "
-                                            f"has been sent to your friend: {recipient_name}, @ {recipient_email}\nThe"
-                                            f" Birthday Wisher.")
-                    connection.close()
-                print('Messages Sent!')
+                try:
+                    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+                        connection.starttls()
+                        connection.login(user=email, password=password)
+                        connection.sendmail(from_addr=email,
+                                            to_addrs=recipient_email,
+                                            msg=f"Subject: Happy Birthday!\n\nHey {recipient_name},\n{message}{wisher_name}."
+                                            )
+                        connection.sendmail(from_addr=email,
+                                            to_addrs=wisher_email,
+                                            msg=f"Subject: Birthday Wish Sent.\n\nHey {wisher_name},\nA birthday wish "
+                                                f"has been sent to your friend: {recipient_name}, @ {recipient_email}\nThe"
+                                                f" Birthday Wisher.")
+                        connection.close()
+                    print('Messages Sent!')
+                except Exception as e:
+                    print(f"An error occurred: {e}")
+                    message = f"Troubleshoot The Birthday Wisher.\nThe error message:\n{e}"
+                    try:
+                        client = Client(os.environ.get('A_ID'), os.environ.get('A_T'))
+                        client.messages.create(
+                            body=message,
+                            from_=os.environ.get('S_ID'),
+                            to=os.environ.get('T_ID')
+                        )
+                    except Exception as e:
+                        print(f"Error sending text message: {e}")
